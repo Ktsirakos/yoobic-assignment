bcd8f7960571d4ecd34a2dd1d208018f63e6b96f
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2020-09-06 16:15:19 +0300

    Component parameters and type checking
    Provided array parameter from pure HTML-JS and added type checking on the arrays contents

--- yoobic-basic-grid/src/components/my-component/my-component.tsx
+++ yoobic-basic-grid/src/components/my-component/my-component.tsx
@@ -9,21 +9,35 @@ export class MyComponent {
   //Declaring main prop as array
   //Type any because the goal is to print
   //Numbers - Strings - Objects - mixed Arrays
-  @Prop() array: any[] = undefined
+  @Prop() array: any[]
 
 
   //Prop for letting the component know which field to display
   //if the array contains objects
-  @Prop() display_key: string = undefined
+  @Prop() display_key: string
 
   //Helper function for creating the list which will be displayed
   //Different function for Number-String and Objects needed!
   private outputList() {
-    let data = [
-      "10", "20", "30", "40"
-    ]
+    return this.array.map((d) => {
+      return this.format(d)
+    });
+  }
 
-    return data.map((d) => <li key={d}>{d}</li>);
+  format(d: any): HTMLElement {
+    let returnValue: any;
+    if (typeof d ==  'number') {
+      returnValue = "number";
+      console.log("is Number");
+    } else if (typeof d == 'string') {
+      returnValue = "string";
+      console.log("is String");
+    } else {
+      returnValue = "object";
+      console.log("is Object");
+    }
+
+    return <li>{returnValue}</li>
   }
 
   render() {
